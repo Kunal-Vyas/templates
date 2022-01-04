74ef278354cdbbd46f28ba22e24bf69a36082741
Kunal Vyas <Kunal.Vyas@quaddimension.ca>
2022-01-03 22:15:38 -0400

    Enabled routing

--- svelte/server.js
+++ svelte/server.js
@@ -2,28 +2,28 @@ const path = require('path'),
   express = require('express'),
   app = express(),
   log4js = require('./configs/log.config.js'),
-  logger = log4js.getLogger('server')
+  logger = log4js.getLogger('server');
 
 // configure local environment
-require('dotenv').config()
+require('dotenv').config();
 
 // serve static files
-app.use('/static', express.static(path.join(__dirname, 'public')))
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST body
 app.use(express.urlencoded({extended: true}));
-app.use(express.json()) // To parse the incoming requests with JSON payloads
+app.use(express.json()); // To parse the incoming requests with JSON payloads
 
 // return index.html as default static page
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 // define port for application to listen on
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-logger.info('Svelte application starting on port: ' + port)
+logger.info('Svelte application starting on port: ' + port);
 
 // start the application server
 app.listen(port,
-  () => console.log(`Svelte started at http://localhost:${port}`))
+  () => console.log(`Svelte started at http://localhost:${port}`));
---
+++ svelte/src/user/home.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte";
+
+  let username = 'world';
+	export let params;
+
+  onMount(async () => {
+    username = params.username;
+  });
+</script>
+
+<div class="container">
+  <main>
+    <h1>Hello {username}!</h1>
+  </main>  
+</div>
