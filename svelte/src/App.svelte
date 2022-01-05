7cffaba87da7df3495f1650ade29b7da1febeb21
Kunal Vyas <Kunal.Vyas@quaddimension.ca>
2022-01-04 21:34:43 -0400

    Fixed routing and navigation, implemented 404

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,16 +1,27 @@
 <script>
 	import router from "page";
-	import Index from "./public/index.svelte";
 	import Home from "./user/home.svelte";
+	import Index from "./public/index.svelte";
+	import PageNotFound from "./public/404.svelte";
 
 	let page, params;
+	let user = {};
 
 	router('/', () => page = Index);
-	router('/home/:username', (ctx, next) => {
-  	params = ctx.params
-  	next()}, () => page = Home);
+	router('/home/:username', 
+		(ctx, next) => {
+			if(!user) {
+				router.redirect('/'); // login
+			} else {
+				params = ctx.params;
+  			next();
+			}
+		}, () => page = Home);
+	
+	// catch-all 404
+	router('/*', () => page = PageNotFound)
 
 	router.start();
 </script>
 
-<svelte:component this={page} />
+<svelte:component this={page} params={params} />
